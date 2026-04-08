8fa18659e297fc0bf114a3c6c68fdec439db977c
JakubKap <jakubkapusta39@gmail.com>
2026-04-08 11:50:28 +0200

    feature(microservices): upgrade zipkin + add notification service

--- customer/src/main/java/com/mycompany/customer/CustomerConfig.java
+++ customer/src/main/java/com/mycompany/customer/CustomerConfig.java
@@ -1,5 +1,6 @@
 package com.mycompany.customer;
 
+import org.springframework.boot.restclient.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +10,7 @@ import org.springframework.web.client.RestTemplate;
 public class CustomerConfig {
     @Bean
     @LoadBalanced
-    public RestTemplate restTemplate(){
-        return new RestTemplate();
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
     }
 }
